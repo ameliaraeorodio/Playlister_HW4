dcbc24574613fe48f258c83c2e00cd2fab369f84
amelia rae orodio <ameliarae.orodio@stonybrook.edu>
2022-11-01 01:39:25 -0400

    adding list for a user

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -40,6 +40,9 @@ const HomeScreen = () => {
     return (
         <div id="playlist-selector">
             <div id="list-selector-heading">
+                <Typography variant="h2">Your Lists</Typography>
+            </div>
+            <div>
             <Fab 
                 color="primary" 
                 aria-label="add"
@@ -48,7 +51,6 @@ const HomeScreen = () => {
             >
                 <AddIcon />
             </Fab>
-                <Typography variant="h2">Your Lists</Typography>
             </div>
             <div id="list-selector-list">
                 {
